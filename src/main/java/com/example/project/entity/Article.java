6cae6fa75229fef54ae2d8661d38eac4214bbfe7
Owner <94shinshinhy@gmail.com>
2022-06-14 00:28:50 +0900

    @OneToMany

--- src/main/java/com/example/project/entity/Article.java
+++ src/main/java/com/example/project/entity/Article.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity // DB가 해당 객체를 인식 가능하게 해줌
 @AllArgsConstructor
@@ -13,7 +15,6 @@ import javax.persistence.*;
 @ToString
 @Getter
 public class Article {
-
     @Id // 대표값을 지정 like a 주민등록번호
     @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성해줌
     private Long id;
@@ -23,6 +24,14 @@ public class Article {
 
     @Column
     private String content;
+
+    /* fetch : LAZY는 지연 로딩, EAGER는 즉시 로딩, @xxToMany의 디폴트는 LAZY, @xxToOne의 디폴트는 EAGER
+       mappedBy : 양방향 연관관계일 때 반대쪽에 매핑되는 엔티티의 필드값
+       cascade : 영속성전이(특정 엔티티를 영속 상태로 만들 때 연관된 엔티티도 함계 영속 상태로 전이됨
+       orphanRemoval : 고아 객체를 삭제해주는 역할
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> Comment = new ArrayList<>();
+    */
 
     public void patch(Article article){
         if(article.title != null) this.title = article.title;
